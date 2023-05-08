a010aec89a45f9253b4f14017a5febf86a01bcb7
Cristian Torres Bogota <crtorres@student.42madrid.com>
2023-05-08 14:57:22 +0200

    firsts files

---
+++ philo.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: crtorres <crtorres@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/05 12:57:00 by crtorres          #+#    #+#             */
+/*   Updated: 2023/05/05 15:00:02 by crtorres         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <pthread.h>
+# include <sys/time.h>
+# include <unistd.h>
+# include <string.h>
+
+#endif
