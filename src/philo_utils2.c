ab17186355c5c216165700d6b5c4e5c5165f0704
Cristian Torres Bogota <crtorres@student.42madrid.com>
2023-05-12 18:37:31 +0200

    data in ft_init_data function added

--- src/philo_utils2.c
+++ src/philo_utils2.c
@@ -6,7 +6,7 @@
 /*   By: crtorres <crtorres@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/12 12:20:50 by crtorres          #+#    #+#             */
-/*   Updated: 2023/05/12 15:25:44 by crtorres         ###   ########.fr       */
+/*   Updated: 2023/05/12 18:15:03 by crtorres         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 int	ft_init_data(t_data *data, char **argv, int argc)
 {
-    data->nbr_philo = ft_atoi(argv[1]);
-    data->philo_id = malloc(sizeof(pthread_t) * data->nbr_philo);
-    if (!data->philo_id)
-        exit_error("alloc memory philo failed\n");
-    data->death_time = (unsigned long)ft_atoi(argv[2]);
-    data->philo = malloc(sizeof(t_philo) * data->nbr_philo);
-    if (!data->philo)
-    
+	data->nbr_philo = ft_atoi(argv[1]);
+	data->philo_id = malloc(sizeof(pthread_t) * data->nbr_philo);
+	if (!data->philo_id)
+		exit_error("alloc memory philo_id failed\n");
+	data->death_time = (unsigned long)ft_atoi(argv[2]);
+	data->philo = malloc(sizeof(t_philo) * data->nbr_philo);
+	if (!data->philo)
+		exit_error("alloc memory philo failed\n");
+	data->eat_time = (unsigned long)ft_atoi(argv[3]);
+	data->sleep_time = (unsigned long)ft_atoi(argv[4]);
+	if (argc == 6)
+		data->nbr_meals = ft_atoi(argv[5]);
+	
 }
